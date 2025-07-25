79f993be5550ee0479ffdd52ad2557dd1097442e
dmex <dmex04@gmail.com>
2025-07-26 05:08:31 +1000

    Add PHNT_25H2

--- phnt/include/phnt.h
+++ phnt/include/phnt.h
@@ -55,6 +55,7 @@
 #define PHNT_WINDOWS_11_22H2 115 // September, 2022  // Build 22621
 #define PHNT_WINDOWS_11_23H2 116 // October, 2023    // Build 22631
 #define PHNT_WINDOWS_11_24H2 117 // October, 2024    // Build 26100
+#define PHNT_WINDOWS_11_25H2 117 // October, 2025    // Build 26200
 #define PHNT_WINDOWS_NEW ULONG_MAX
 
 #ifndef PHNT_MODE
